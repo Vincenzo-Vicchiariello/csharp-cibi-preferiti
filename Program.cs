29d45d348ed2485b93e9b29b095240b18992ad42
Vincenzo-Vicchiariello <v.vicchiariello@outlook.it>
2023-04-14 18:10:06 +0200

    Non riesco a proseguire col bonus nè a integrare con le funzioni, provo nel week end.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Ecco la classifica dei miei cibi preferiti!");
+/*
+ * Console.WriteLine("Ecco la classifica dei miei cibi preferiti!");
 string[] favFoods = { "Tiramisù", "Pizza", "Cioccolata alla menta", "Uva rossa", "Parmigiana di melanzane" };
 int foodsListLength = favFoods.Length;
 
@@ -13,4 +14,28 @@ for (int i = 0; i < foodsListLength; i++)
 Console.WriteLine($"Il mio cibo top è {favFoods[0]}");
 
 Console.WriteLine($"Il mio cibo preferito ma non troppo è {favFoods[foodsListLength - 1]}");
+
+Funziona anche senza le funzioni, non riesco a capire come dovrei integrarle.
+*/
+using System.Drawing;
+
+Console.WriteLine("Quanti cibi preferiti hai?");
+int favFoodsAmount = int.Parse(Console.ReadLine());
+
+
+Console.WriteLine(favFoodsAmount);
+
+string[] foodList = new string[favFoodsAmount];
+
+for (int i = 0; i < foodList.Length; i++)
+{
+    Console.WriteLine("Dimmi un cibo");
+    string food = Console.ReadLine();
+    foodList[i] = food;
+}
+
+for (int i = 0; i < foodList.Length; i++)
+{
+    Console.WriteLine($"-{i + 1}.  {foodList[i]}"); 
+}
 
