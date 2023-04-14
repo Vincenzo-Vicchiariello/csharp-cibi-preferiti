2d29936f70957392488d6f5ae246a4c7361419d2
Vincenzo-Vicchiariello <v.vicchiariello@outlook.it>
2023-04-14 15:32:03 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ecco la classifica dei miei cibi preferiti!");
+string[] favFoods = { "Tiramisù", "Pizza", "Cioccolata alla menta", "Uva rossa", "Parmigiana di melanzane" };
+int foodsListLength = favFoods.Length;
+
+Console.WriteLine(foodsListLength);
+
+for (int i = 0; i < foodsListLength; i++)
+{
+    Console.WriteLine($"-{i + 1}.   {favFoods[i]}");
+}
+
+Console.WriteLine($"Il mio cibo top è {favFoods[0]}");
+
+Console.WriteLine($"Il mio cibo preferito ma non troppo è {favFoods[foodsListLength - 1]}");
+
